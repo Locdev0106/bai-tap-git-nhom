61db1cd17a592b2d9d87549981179158d865d6ba
NguyenDinhLoc <nguyendinhloc010605@gmail.com>
2026-06-18 20:49:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def tinh_thue_thu_nhap(thu_nhap):
-    # Dev A áp dụng giảm trừ 4 trieu va thue 5%
-    thu_nhap_tinh_thue = thu_nhap - 4000000
-    return max(0, thu_nhap_tinh_thue * 0.1)
-    # Dev B áp dụng mức thuế 10%
-print("Thuế phải nộp của bạn là:", tinh_thue_thu_nhap(10000000))
+def tinh_diem_gpa(diem_so):
+    #TODO :Cần cập nhật logic quy đổi sang hệ 4
+    return 0.0
+print("Điểm GPA của hệ 4 là:",tinh_diem_gpa(8.5))
